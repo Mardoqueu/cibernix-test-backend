9d165e97f8d61804d7195417312e11d869652ed6
Mardoqueu Sousa <mdq767@gmail.com>
2022-12-02 16:33:10 -0300

    retornando dto para user resource

--- src/main/java/br/com/mardoqueu/cibernixtestebackend/domain/dto/UserDTO.java
+++ src/main/java/br/com/mardoqueu/cibernixtestebackend/domain/dto/UserDTO.java
@@ -1,11 +1,13 @@
 package br.com.mardoqueu.cibernixtestebackend.domain.dto;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Setter
+@Setter @Getter
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserDTO {
@@ -13,5 +15,7 @@ public class UserDTO {
     private Integer id;
     private String name;
     private String email;
+
+    @JsonIgnore
     private String password;
 }
--- src/main/java/br/com/mardoqueu/cibernixtestebackend/resources/UserResource.java
+++ src/main/java/br/com/mardoqueu/cibernixtestebackend/resources/UserResource.java
@@ -1,7 +1,8 @@
 package br.com.mardoqueu.cibernixtestebackend.resources;
 
-import br.com.mardoqueu.cibernixtestebackend.domain.Users;
+import br.com.mardoqueu.cibernixtestebackend.domain.dto.UserDTO;
 import br.com.mardoqueu.cibernixtestebackend.services.UserService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,10 +15,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserResource {
 
     @Autowired
+    private ModelMapper mapper;
+
+    @Autowired
     private UserService service;
 
     @GetMapping(value = "/{id}")
-    public ResponseEntity<Users> findById(@PathVariable Integer id) {
-        return ResponseEntity.ok().body(service.findById(id));
+    public ResponseEntity<UserDTO> findById(@PathVariable Integer id) {
+        return ResponseEntity.ok().body(mapper.map(service.findById(id), UserDTO.class));
     }
 }
