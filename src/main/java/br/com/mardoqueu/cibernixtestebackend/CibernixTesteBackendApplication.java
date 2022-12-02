4e2b6ab812f57161039e137b9f97d756ca32523e
Mardoqueu Sousa <mdq767@gmail.com>
2022-12-02 13:05:40 -0300

    Classe de dominio

--- src/main/java/br/com/mardoqueu/cibernixtestebackend/CibernixTesteBackendApplication.java
+++ src/main/java/br/com/mardoqueu/cibernixtestebackend/CibernixTesteBackendApplication.java
@@ -1,5 +1,6 @@
 package br.com.mardoqueu.cibernixtestebackend;
 
+import br.com.mardoqueu.cibernixtestebackend.domain.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CibernixTesteBackendApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(CibernixTesteBackendApplication.class, args);
+
+        User user = new User(1, "mardoqueu", "mardoqueu@mail.com","123");
     }
 
 }
---
+++ src/main/java/br/com/mardoqueu/cibernixtestebackend/domain/User.java
@@ -0,0 +1,20 @@
+package br.com.mardoqueu.cibernixtestebackend.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+    private String password;
+}
