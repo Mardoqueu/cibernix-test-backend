4a9631c3b9062b27cf13874b105a580de686f7f8
Mardoqueu Sousa <mdq767@gmail.com>
2022-12-02 12:08:11 -0300

    Project initiation

---
+++ src/main/java/br/com/mardoqueu/cibernixtestebackend/CibernixTesteBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.mardoqueu.cibernixtestebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CibernixTesteBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CibernixTesteBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/mardoqueu/cibernixtestebackend/CibernixTesteBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mardoqueu.cibernixtestebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CibernixTesteBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
