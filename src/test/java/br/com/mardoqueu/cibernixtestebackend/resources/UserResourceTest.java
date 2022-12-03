f45c015988761bb520da379130c00071a577e411
Mardoqueu Sousa <mdq767@gmail.com>
2022-12-03 19:21:02 -0300

    teste para endpoint findAll da classe use resource implementado

--- src/test/java/br/com/mardoqueu/cibernixtestebackend/resources/UserResourceTest.java
+++ src/test/java/br/com/mardoqueu/cibernixtestebackend/resources/UserResourceTest.java
@@ -10,7 +10,11 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -26,6 +30,7 @@ class UserResourceTest {
     public static final String NAME     = "Mardoqueu";
     public static final String EMAIL    = "mardoqueu567@gmail.com";
     public static final String PASSWORD = "123";
+    public static final int INDEX = 0;
 
     private Users users;
     private UserDTO userDTO;
@@ -63,7 +68,23 @@ class UserResourceTest {
         assertEquals(PASSWORD, response.getBody().getPassword());
     }
     @Test
-    void findAll() {
+    void whenFindAllThenReturnAListOfUserDTO() {
+        when(service.findAll()).thenReturn(List.of(users));
+        when(mapper.map(any(), any())).thenReturn(userDTO);
+
+        ResponseEntity<List<UserDTO>> response = resource.findAll();
+
+        assertNotNull(response);
+        assertNotNull(response.getBody());
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(ResponseEntity.class, response.getClass());
+        assertEquals(ArrayList.class, response.getBody().getClass());
+        assertEquals(UserDTO.class, response.getBody().get(INDEX).getClass());
+
+        assertEquals(ID, response.getBody().get(INDEX).getId());
+        assertEquals(NAME, response.getBody().get(INDEX).getName());
+        assertEquals(EMAIL, response.getBody().get(INDEX).getEmail());
+        assertEquals(PASSWORD, response.getBody().get(INDEX).getPassword());
     }
 
     @Test
