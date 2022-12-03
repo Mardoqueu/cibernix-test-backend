8aca46ce443409ae58425291d3d0df875a68e5bb
Mardoqueu Sousa <mdq767@gmail.com>
2022-12-03 16:50:00 -0300

    teste para a classe  users service metodo create com sucesso

--- src/test/java/br/com/mardoqueu/cibernixtestebackend/services/impl/UserServiceImplTest.java
+++ src/test/java/br/com/mardoqueu/cibernixtestebackend/services/impl/UserServiceImplTest.java
@@ -16,6 +16,7 @@ import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.when;
 
@@ -91,7 +92,17 @@ class UserServiceImplTest {
     }
 
     @Test
-    void create() {
+    void whenCreateThenReturnSucess() {
+        when(repository.save(any())).thenReturn(users);
+
+        Users response = service.create(userDTO);
+
+        assertNotNull(response);
+        assertEquals(Users.class, response.getClass());
+        assertEquals(ID, response.getId());
+        assertEquals(NAME, response.getName());
+        assertEquals(EMAIL, response.getEmail());
+        assertEquals(PASSWORD, response.getPassword());
     }
 
     @Test
